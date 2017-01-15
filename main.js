e8fbf6ad862dce934097aa7e48b99fcdf5dfa78b
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-01-15 13:17:08 -0500

    Refactor: drawReect, drawAll and moveAll

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var canvas, context;
+var canvasWidth = 800;
+var canvasHeight = 600;
 
 var ballX = 0;
 var ballY = 0;
 
-var ballSpeedX = 2;
-var ballSpeedY = 5;
+var ballSpeedX = 1;
+var ballSpeedY = 2;
 
 window.onload = function() {
     // for 2d context, canvas.width and canvas.height
     canvas = document.getElementById('gameBoard');
-    canvas.width = 800;
-    canvas.height = 600;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
 
     context = canvas.getContext('2d');
 
@@ -21,27 +23,44 @@ window.onload = function() {
 
 function updateAll() {
 
-    // reset and paint background
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
     // draw ball and movements
-    drawBall();
+    drawAll();
+    moveAll();
+
 
 }
 
-function drawBall() {
-    ballX += ballSpeedX;
-    ballY += ballSpeedY;
-    if (ballX > canvas.width || ballX < 0) {
-        ballSpeedX = -ballSpeedX;
-    }
-    if (ballY > canvas.height || ballY < 0) {
-        ballSpeedY = -ballSpeedY;
-    }
+function drawAll() {
+    // reset and paint background
+    drawRect(0, 0, canvas.width, canvas.height, 'black');
 
+    // draw ball
     context.fillStyle = 'white';
     context.beginPath();
     context.arc(ballX, ballY, 10, 0, 2 * Math.PI, true);
     context.fill();
+
+}
+
+function drawRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
+    context.fillStyle = fillColor;
+    context.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+
+}
+
+function moveAll() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+    if (ballX > canvas.width) {
+        ballSpeedX *= -1;
+    }
+    if (ballX < 0) {
+        ballSpeedX *= -1;
+    }
+    if (ballY > canvas.height) {
+        ballSpeedY *= -1;
+    }
+    if (ballY < 0) {
+        ballSpeedY *= -1;
+    }
 }
